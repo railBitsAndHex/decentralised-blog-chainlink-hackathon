b4cafe36e7f8d7dbd2900f9976f7189bcec048f8
bits predicted <104562337+railBitsAndHex@users.noreply.github.com>
2022-05-14 18:00:22 +0800

    Renamed states and types files

--- client/src/states/AuthContext.s.ts
+++ client/src/states/AuthContext.s.ts
@@ -1,12 +1,12 @@
-import { AuthContextInterface } from './../types/authContext_d_types';
+import { AuthContextInterface } from '../types/authContext';
 const AuthStateInitial : AuthContextInterface = {
     isAuthenticated: false,
     accounts: [],
     error: "",
     login: () => console.log("Login from context"),
     threeIdLogin: () => console.log("Three Id login from context"),
-    logout: () => console.log("Logout from context")
-
+    logout: () => console.log("Logout from context"),
+    setError: (err: string) => console.log(err)
 }
 
 export {AuthStateInitial}
